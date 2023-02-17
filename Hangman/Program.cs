ad250d6f21fc5571f883a7c4064937b50f8c509e
Skinlay <icaitlin02@live.nl>
2023-02-17 18:22:50 +0100

    it works

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -7,21 +7,25 @@ namespace Hangman
         static void Main(string[] args)
         {
             input_word();
-            under_score();
-            gess();
+            while (lives > 0)
+            {
+                under_score();
+                gess();
+            }
+
         }
 
-        public static string answer { get; set; } = "";
         public static string user_word { get; set; } = "";
         public static int user_word_length { get; set; } = 0;
         public static string word_lines { get; set; } = "";
         public static int word_line_count { get; set; } = 0;
-        //public static var letter { get; set; } = "";
         public static string user_gess { get; set; } = "";
         public static int lives { get; set; } = 10;
 
-        public static List<char> letter = new List<char>();
-
+        public static List<char> answer = new List<char>();
+        public static int user_gess_length { get; set; } = 0;
+        public static char user_gess_char { get; set;}
+        public static List<char> Correct_gess = new List<char>();
 
         static void input_word()
         {
@@ -34,7 +38,7 @@ namespace Hangman
                 if (key.Key == ConsoleKey.Enter)
                     break;
                 user_word += key.KeyChar;
-                //letter.Add(key.KeyChar);
+                //answer.Add(key.KeyChar);
 
                 //couts the lengt of het users input
                 user_word_length = user_word.Length;
@@ -42,15 +46,13 @@ namespace Hangman
             //put each letter of the ginven word in a list
             // letter is a local varialbal is this the reason  it cant be call outside the loop?
 
-            //List<char> letter = new List<char>();
-            letter.AddRange(user_word);
-
-            
+            //List<char> answer = new List<char>();
+            answer.AddRange(user_word);            
 
             //print the list of letters
-            Console.WriteLine(string.Join(" ", letter));
-            //Console.WriteLine(letter);
-            Console.WriteLine(user_word + " " + user_word_length);
+            Console.WriteLine(string.Join(" ", answer));
+            //Console.WriteLine(answer);
+            Console.WriteLine("user_word = " + user_word + "  user_word_length = " + user_word_length);
         }
         static void under_score()
         {
@@ -62,11 +64,43 @@ namespace Hangman
             }
             Console.WriteLine(word_lines);
         }
-
+        //als de lente langer is dan 1 -> contorleer de heel worden --> correct? -> einde game -- incorect? - 1 leven, raad opnieuw 
+        //is de lengte 1 lang --> controleer of de letter in het woord zit --> correct? zet woord in lijst met letters --> raad op nieuw.
+        //              incorect? -1 leven --> sla letter op in lijst, -->
+        //              levens 0? nee ga verder -- ja, einde geme verlooren.
+        //
          static void gess()
         {
             user_gess = Console.ReadLine();
-            Console.WriteLine(user_gess);
+            Console.WriteLine("user_gess = " + user_gess);
+            
+            user_gess_length = user_gess.Length;
+
+            if (user_gess_length == 1)
+            {
+                user_gess_char = Convert.ToChar(user_gess);
+                Console.WriteLine("user_gess_char = " + user_gess_char);
+                if (answer.Contains(user_gess_char))
+                {
+                    Console.WriteLine("nice");
+                    Correct_gess.Add(user_gess_char);
+                }
+                else
+                {
+                    lives -= 1;
+                    Console.WriteLine();
+                }
+            }
+            else if (user_gess == user_word)
+            {
+                Console.WriteLine("noice");
+            }
+            else
+            {
+                Console.WriteLine("nope");
+                lives -= 1;
+            }
+            Console.WriteLine(lives);
         }
         //user input - word
         //make input invisebel word
